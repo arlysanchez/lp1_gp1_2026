a87102da751f2a947b17459d02169b6050eb765d
LAB 2 <LAB 2@LAB2-36>
2026-05-07 16:03:48 -0500

    metodo de register implementado y testeado.

---
+++ test/Test/TestPersona.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Test;
+
+import Dao.PersonaDaoImpl;
+import Interface.IPersona;
+import Model.Persona;
+import Model.Usuario;
+
+/**
+ *
+ * @author LAB 2
+ */
+public class TestPersona {
+
+    IPersona dao = new PersonaDaoImpl();
+
+    public static void main(String[] args) {
+        TestPersona t = new TestPersona();
+        t.crear_usuario();
+    }
+
+    public void crear_usuario() {
+        Persona p = new Persona();
+        p.setNombre("Jhan Arly");
+        p.setEmail("jhan@gmail.com");
+        p.setDireccion("upeu");
+        p.setTelefono("987654321");
+        Usuario u = new Usuario();
+        u.setPassword("admin123");
+        int result = dao.insertar(p, u);
+        if (result > 0) {
+            System.out.println("Usuario" + p.getEmail());
+            System.out.println("Rol" + u.getRol());
+        } else {
+            System.out.println("No se realizo el registro");
+        }
+    }
+
+}
